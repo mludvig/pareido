308b6c5bfba701893f3f0fc0a7cd4c0912774f57
Michael Ludvig <mludvig@logix.net.nz>
2021-09-24 16:57:28 +1200

    Support JSON and Image outputs

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import hashlib
+import time
 import json
+from io import BytesIO
 
-from flask import Flask, request, Response
+from PIL import Image
+from flask import Flask, request, Response, send_file
 
-from detect import detect
+from detect import detect, filter_detections, draw_detections
 
 app = Flask(__name__)
 
@@ -20,28 +22,47 @@ def index():
     <body>
     <h1>Upload new image</h1>
     <form action="/image" method="post" enctype="multipart/form-data">
-      Min confidence: <input type="text" name="min_confidence" value="50" size="5"><br/>
-      Min area: <input type="text" name="min_area" value="5" size="5"><br/>
+      Min confidence: <input type="text" name="min_confidence" value="50" size="5">%<br/>
+      Min area: <input type="text" name="min_area" value="5" size="1">%<br/>
       Image: <input type="file" name="image"><br/>
-      <input type="submit" value="Upload">
+      <input type="submit" name="output" value="image">
+      <input type="submit" name="output" value="json">
     </form>
     """
 
 @app.route("/image", methods=["POST"])
 def image():
-    # Test with: 'curl -XPOST -F image=@path/to/image.jpg http://...'
-    #print(f"{request.headers=!s}")
-    print(f"{request.form=!s}")
-    if 'image' not in request.files:
-        return json.dumps({"error": "'image' not supplied"}), 400
-    image = request.files['image']
-    min_confidence = int(request.form.get('min_confidence', 0))
-    min_area = int(request.form.get('min_area', 0))
-    result = detect(image, min_confidence, min_area)
-    return Response(json.dumps({
-        "filename": image.filename,
-        "result": result,
-    }, default=lambda o: o.__dict__, indent=2), mimetype="application/json")
+    # Find the parameters either in form data or in query args
+    min_confidence = float(request.form.get('min_confidence', request.args.get('min_confidence', 0)))
+    min_area = float(request.form.get('min_area', request.args.get('min_area', 0)))
+    output = request.form.get('output', request.args.get('output', 'json'))
+
+    results = []
+    for _file in request.files:
+        # Open the image in PIL format
+        image = Image.open(request.files[_file]).convert("RGB")
+        # Time the detection
+        start_ts = time.time()
+        detections = detect(image)
+        elapsed_sec = time.time() - start_ts
+        detections = filter_detections(image, detections, min_confidence, min_area)
+
+        # Draw the results if output == "image"
+        if output == "image":
+            image_new = draw_detections(image, detections)
+            image_buf = BytesIO()
+            image_new.save(image_buf, 'JPEG', quality=80)
+            image_buf.seek(0)
+            return send_file(image_buf, mimetype="image/jpeg")
+
+        # Else send detections as JSON
+        results.append({
+            "filename": request.files[_file].filename,
+            "elapsed_sec": elapsed_sec,
+            "detections": detections,
+        })
+
+    return Response(json.dumps(results), mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- server/detect.py
+++ server/detect.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from openvino_person_detection_retail import InferenceModel  # import the AI Model
 from modelplace_api import Device
 
 model = InferenceModel()        # Load once
 model.model_load(Device.cpu)
 
-def detect(image, min_confidence=0, min_area=0):
-    image = Image.open(image).convert("RGB")
-    results = model.process_sample(image)
-    return filter_results(image, results, min_confidence, min_area)
+def detect(image):
+    #image = Image.open(image).convert("RGB")
+    detections = model.process_sample(image)
+    return detections
 
-def filter_results(image, results, min_confidence, min_area):
+def filter_detections(image, detections, min_score, min_area):
     out = []
-    for result in results:
-        result = dict(result)
-        result["score"] *= 100  # We use 0%-100% score
-        if result["score"] < min_confidence:
-            print(f"Low score: {result['score']}")
+    for detection in detections:
+        detection = dict(detection)
+        detection["score"] *= 100  # We use 0%-100% score
+        if detection["score"] < min_score:
+            print(f"Low score: {detection['score']}")
             continue
         image_area = image.size[0]*image.size[1]
-        result_area = (result["x2"]-result["x1"]) * (result["y2"]-result["y1"])
-        result["area"] = 100*result_area/image_area
-        if result["area"] < min_area:
-            print(f"Small area: {result['area']}")
+        detection_area = (detection["x2"]-detection["x1"]) * (detection["y2"]-detection["y1"])
+        detection["area"] = 100*detection_area/image_area
+        if detection["area"] < min_area:
+            print(f"Small area: {detection['area']}")
             continue
-        out.append(result)
+        out.append(detection)
     return out
+
+def draw_detections(image, detections):
+    line_width = 2
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("FreeMono.ttf", 15)
+    for d in detections:
+        rect = (d['x1'], d['y1'], d['x2'], d['y2'])
+        draw.rectangle(rect, outline='lightgreen', width=line_width)
+
+        text = f"{d['score']:.1f}%"
+        text_size = font.getsize(text)
+        if d['y1'] < text_size[1]:
+            # place label above detection
+            text_rect = (d['x1']+line_width, d['y1']+line_width, d['x1']+text_size[0]+line_width, d['y1']+text_size[1]+line_width)
+        else:
+            # place label inside detection
+            text_rect = (d['x1'], d['y1']-1-text_size[1], d['x1']+text_size[0], d['y1']-1)
+        draw.rectangle(text_rect, outline='yellow', fill='yellow')
+        draw.text(text_rect[:2], text, fill='black', font=font)
+    return image
